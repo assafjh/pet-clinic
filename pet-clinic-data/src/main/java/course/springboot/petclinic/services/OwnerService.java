63723e2c202452b7ea9c2f3db42d68d11bfc2220
hazana <assaf.hazan@rsa.com>
2019-07-28 15:22:16 +0300

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/course/springboot/petclinic/services/OwnerService.java
@@ -0,0 +1,13 @@
+package course.springboot.petclinic.services;
+
+import course.springboot.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/course/springboot/petclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package course.springboot.petclinic.services;
+
+import course.springboot.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
